03e4872c2b34b4990e6b64f2ad4d749bb95c5026
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-25 21:10:06 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const nottifocation = document.createElement("div");
+    document.querySelector("body").append(nottifocation);
+    nottifocation.classList.add("notification");
+    nottifocation.classList.add(type);
+    nottifocation.style.top = `${posTop}px`;
+    nottifocation.style.right = `${posRight}px`;
+    nottifocation.insertAdjacentHTML("beforeend", `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>`);
+    setTimeout(()=>{
+        nottifocation.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
